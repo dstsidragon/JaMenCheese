dd147dd6644f342f540d27b232898d27845b34ad
DESKTOP-9P92TIH\LucienLo <sim322on@yahoo.com.tw>
2021-07-27 12:20:10 +0800

    redi vue config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,13 @@
 // 該檔案生成的方式可以在 Vue Ui 中的「設定」->「Vue Cli」->「公開路徑」找到。
 module.exports = {
-  // 預設狀況下，部署到 GitHub Pages 可能會無法顯示畫面
-  // 因此需要多設置該屬性
-  // 官方文件：https://cli.vuejs.org/zh/config/#publicpath
-  publicPath: './',
   transpileDependencies: ['@vue/reactivity'],
   chainWebpack: (config) => {
     config.plugin('html')
       .tap((args) => {
-        // eslint-disable-next-line no-param-reassign
-        args[0].title = '呷面起士|勾起你心中的餓';
+        const [obj] = args; // 陣列解構
+        obj.title = '呷面起士|勾起你心中的餓';
         return args;
       });
   },
+  publicPath: '/呷面起士|勾起你心中的餓/',
 };
